c0762f82ccad9ca1819469e995a9b3aacedff6f5
Caio Souza <caiopsouza1@gmail.com>
2023-03-15 15:57:34 -0300

    Criação dos botões e formatação basica do contador

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('0')
+let count = 0
+const value = document.getElementById("value")
+const decreaseBtn = document.getElementById("btn-decrease")
+const increaseBtn = document.getElementById("btn-increase")
+const resetBtn = document.getElementById("btn-reset")
+
+
+console.log(counter)
