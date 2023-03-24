76cfeef4f1d9f4bd46ec276d28b1c23203f9e473
Caio Souza <caiopsouza1@gmail.com>
2023-03-24 13:49:32 -0300

    Contador funcional e css criado

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const value = document.getElementById("value")
-const increaseBtn = document.getElementById("btn-increase")
-const decreaseBtn = document.getElementById("btn-decrease")
-
 let count = 0
-value.innerHTML = count
 
-function increaseBtn () {
-count++;
-value.innerHTML = count
+document.getElementById("btnDecrease").onclick = function() {
+    count -= 1
+    document.getElementById("value").innerHTML = count
+    console.log(count)
 }
 
+document.getElementById("btnReset").onclick = function() {
+    count = 0
+    document.getElementById("value").innerHTML = count
+    console.log(count)
+}
+
+document.getElementById("btnIncrease").onclick = function() {
+    count += 1
+    document.getElementById("value").innerHTML = count
+    console.log(count)
+}
